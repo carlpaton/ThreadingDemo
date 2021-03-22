c57b11dc6b64bfff7e45ad89c3f3701e12ccfa64
Carl <carl.paton@gmail.com>
2021-03-23 08:56:20 +1300

    init: hosted service

---
+++ src/HostedService.Ca/FooService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedService.Ca
+{
+    public class FooService : BackgroundService
+    {
+        private readonly int _millisecondsTimeout;
+
+        public FooService()
+        {
+            _millisecondsTimeout = 2000; // the value would have been injected with IOptions/Settings
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //_logger.LogDebug($"FooService is starting.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //_logger.LogDebug($"FooService task doing background work.");
+
+                SimulateSomeWorker();
+
+                //await Task.Delay(_settings.CheckUpdateTime, stoppingToken);
+                await Task.Delay(_millisecondsTimeout);
+            }
+
+            //_logger.LogDebug($"GracePeriod background task is stopping.");
+        }
+
+        private void SimulateSomeWorker()
+        {
+            Console.WriteLine("Worker was run {0}", DateTime.Now);
+        }
+    }
+}
